a9d3136efe0432bc9462f649aafba7b66bc28725
nikizhelqzkov <nikizhelqzkov@gmail.com>
2020-10-24 22:01:45 +0300

    transform normal notation to rev pol not with ( )

--- stack/main.cpp
+++ stack/main.cpp
@@ -7,7 +7,29 @@ bool isItNumber(char c)
 {
     return c >= '0' && c <= '9';
 }
-
+std::string normalToRevPolNot(const std::string &exp)
+{
+    std::stack<char> s;
+    std::string newS;
+    for (char symbol : exp)
+    {
+        if (isItNumber(symbol))
+        {
+            newS.push_back(symbol);
+        }
+        else
+        {
+            s.push(symbol);
+        }
+        while (s.top() == ')' && s.top() != '(')
+        {
+            s.pop();
+            newS.push_back(s.top());
+            s.pop();
+        }
+    }
+    return newS;
+}
 int fromCharToInt(char c)
 {
     if (!isItNumber(c))
@@ -16,11 +38,11 @@ int fromCharToInt(char c)
     }
     return (int)c - '0';
 }
-int result(std::string symbols)
+double result(std::string symbols)
 {
 
-    std::stack<int> results;
-    int res = 0;
+    std::stack<double> results;
+    double res = 0;
     int count = 0;
     while (count <= symbols.size())
     {
@@ -43,7 +65,12 @@ int result(std::string symbols)
 }
 int main()
 {
-    std::string symbols = {'3', '4', '8', '+', '7', '+', '2', '*'};
+    // std::string symbols = {'3', '4', '8', '+', '7', '+', '2', '*'};
 
-    std::cout << result(symbols);
+    //std::cout << result(symbols);
+    std::string r = "((((3+2)*3)-4)/2)";
+    std::cout << r<<std::endl;
+    r = normalToRevPolNot(r);
+    std::cout << r<<std::endl;
+    std::cout << result(r);
 }
