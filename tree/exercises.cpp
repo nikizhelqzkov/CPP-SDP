356bccfe0909397bcdf1d32816de4778b1a10d92
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-01-11 17:20:08 +0200

    more functions for bin tree training

--- tree/exercises.cpp
+++ tree/exercises.cpp
@@ -321,6 +321,18 @@ void print90Deg(TN<T> *root, int n)
     print90Deg(root->left, n);
 }
 template <class T>
+void print90Deg(BinNode<T> *root, int n)
+{
+    if (!root)
+    {
+        return;
+    }
+    n += 5;
+    print90Deg(root->right, n);
+    std::cout << std::setw(n) << root->val << "\n";
+    print90Deg(root->left, n);
+}
+template <class T>
 std::vector<T> listLeaves(TN<T> *root)
 {
     std::queue<TN<T> *> q;
@@ -475,18 +487,187 @@ void DfsTS(TN<T> *root)
         }
     }
 }
+template <class T>
+bool MirrorHelper(BinNode<T> *left, BinNode<T> *right)
+{
+    if (!left && !right)
+    {
+        return true;
+    }
+    if (left && !right)
+    {
+        return false;
+    }
+    if (!left && right)
+    {
+        return false;
+    }
+    if (left->val != right->val)
+    {
+        return false;
+    }
+    return MirrorHelper(left->left, right->right) && MirrorHelper(left->right, right->left);
+}
+template <class T>
+bool isMirrorredTree(BinNode<T> *root)
+{
+    if (!root)
+    {
+        return true;
+    }
+    return MirrorHelper(root->left, root->right);
+}
+template <class T>
+void helper(BinNode<T> *t, std::string &str)
+{
+    if (!t)
+    {
+        return;
+    }
+    str += (t->val);
+    if (t->left)
+    {
+        str += '(';
+        helper(t->left, str);
+        str += ')';
+    }
+    if (t->right)
+    {
+        if (!t->left)
+        {
+            str += "()";
+        }
+        str += '(';
+        helper(t->right, str);
+        str += ')';
+    }
+}
+template <class T>
+std::string tree2str(BinNode<T> *t)
+{
+    std::string s = "";
+    helper(t, s);
+    return s;
+}
+template <class T>
+std::vector<std::vector<T>> levelOrder(BinNode<T> *root)
+{
+    if (!root)
+    {
+        return {{}};
+    }
+    std::vector<std::vector<T>> result;
+    std::queue<BinNode<T> *> q;
+    q.push(root);
+    q.push(nullptr);
+
+    int i = 0;
+    std::vector<T> temp;
+    while (!q.empty())
+    {
+        BinNode<T> *cur = q.front();
+        q.pop();
+        if (!cur)
+        {
+            if (!q.empty())
+            {
+                q.push(cur);
+                ++i;
+            }
+            result.push_back(temp);
+            temp.clear();
+        }
+        else
+        {
+            temp.push_back(cur->val);
+            if (cur->left)
+            {
+                q.push(cur->left);
+            }
+            if (cur->right)
+            {
+                q.push(cur->right);
+            }
+        }
+    }
+    return result;
+}
+template <class T>
+int sumOfEvenRowsWithLeftNode(BinNode<T> *root)
+{
+    if (!root || (!root->left && !root->right))
+    {
+        return 0;
+    }
+    int row = 0;
+    int sum = 0;
+    std::queue<BinNode<T> *> q;
+    q.push(root);
+    q.push(nullptr);
+    while (!q.empty())
+    {
+        BinNode<T> *current = q.front();
+        q.pop();
+        if (!current)
+        {
+            if (!q.empty())
+            {
+                ++row;
+                q.push(current);
+            }
+        }
+        else
+        {
+            if (row % 2 == 0 && current->left && !current->right)
+            {
+                sum += current->val;
+            }
+            if (current->left)
+            {
+                q.push(current->left);
+            }
+            if (current->right)
+            {
+                q.push(current->right);
+            }
+        }
+    }
+}
 int main()
 {
-    // BinNode<char> *t = new BinNode<char>{'a', nullptr, nullptr};
-    // addBinRoad("L", t, 'a');
-    // addBinRoad("LR", t, 'b');
-    // addBinRoad("LRL", t, 'c');
-    // addBinRoad("R", t, 'd');
-    // addBinRoad("RL", t, 'e');
-    // addBinRoad("RLL", t, 'f');
+    BinNode<char> *t = new BinNode<char>{'1', nullptr, nullptr};
+    // addBinRoad("L", t, 'b');
+    // addBinRoad("R", t, 'b');
+    // addBinRoad("LL", t, 'c');
+    // addBinRoad("RR", t, 'c');
+    // addBinRoad("LR", t, 'd');
+    // addBinRoad("RL", t, 'd');
+    // addBinRoad("LLL", t, 'e');
+    // addBinRoad("RRR", t, 'e');
+    // addBinRoad("LLLR", t, 'f');
+    // addBinRoad("RRRL", t, 'f');
+
+    addBinRoad("L", t, '2');
+    addBinRoad("LL", t, '4');
+    addBinRoad("R", t, '3');
+    // print90Deg(t, 0);
+
+    // std::cout << std::boolalpha << isMirrorredTree(t);
+    // std::cout << tree2str(t);
+    std::vector<std::vector<char>> v;
+    v = levelOrder(t);
+    for (auto &&vec : v)
+    {
+        for (auto &&el : vec)
+        {
+            std::cout << el << " ";
+        }
+        std::cout << "\n";
+    }
+
     //bfs2(t);
     //std::vector<std::string> words = levelWords(t);
-    std::vector<std::string> words = {};
+    // std::vector<std::string> words = {};
     // deepWords(t, words, "");
     // print(words);
     // TreeThree<int> *t = new TreeThree<int>{12, nullptr, nullptr};
@@ -504,25 +685,25 @@ int main()
     //TreeNode<int> *t = createTree<int>(10);
     //dfs(t);
     //printLeaves(t);
-    TN<int> *t = new TN<int>{9};
-    addTN(t, 5);
-    addTN(t, 1);
-    addTN(t, 10);
-    addTN(t, 8);
-    addTN(t, 15);
+    // TN<int> *t = new TN<int>{9};
+    // addTN(t, 5);
+    // addTN(t, 1);
+    // addTN(t, 10);
+    // addTN(t, 8);
+    // addTN(t, 15);
     // addTN(t, 0);
     // addTN(t, 95);
     // addTN(t, 1);
     // addTN(t, 3);
     // addTN(t, 78);
-    print90Deg(t, 0);
-    std::cout << "\n";
-    std::vector<int> l = listLeaves(t);
-    for (auto &&el : l)
-    {
-        std::cout << el << " ";
-    }
-    std::cout << "\n"
-              << findTrace(t, 8) << "\n";
-    std::cout << getAt(t, 2);
+    //     print90Deg(t, 0);
+    //     std::cout << "\n";
+    //     std::vector<int> l = listLeaves(t);
+    //     for (auto &&el : l)
+    //     {
+    //         std::cout << el << " ";
+    //     }
+    //     std::cout << "\n"
+    //               << findTrace(t, 8) << "\n";
+    //     std::cout << getAt(t, 2);
 }
